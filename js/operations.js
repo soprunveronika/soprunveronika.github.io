1b79a807095cffb0d6c29ed09361c6260e0aa0fc
soprunveronika <71070202+soprunveronika@users.noreply.github.com>
2023-11-07 18:36:43 +0200

    Update operations.js

--- js/operations.js
+++ js/operations.js
@@ -1,7 +1,7 @@
 window.onload = emptyTablesGenerator
 let N
 let lastN
-const width = 465 // 455 350
+const width = 475 // 455 350
 const height = 310
 const board = document.getElementById("board")
 let COMPLEXITY_TYPE = "MEDIUM_LEVEL"
